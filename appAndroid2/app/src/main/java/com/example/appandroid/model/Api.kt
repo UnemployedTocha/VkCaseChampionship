a7cedb928bde9c8286875f96a8e287ec04957258
irina <i.savelova@g.nsu.ru>
2025-10-04 20:24:08 +0700

    add class App

---
+++ appAndroid2/app/src/main/java/com/example/appandroid/model/Api.kt
@@ -0,0 +1,24 @@
+package com.example.appandroid.model
+
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Screenshot(
+    val id: Int,
+    val url: String
+)
+
+@Serializable
+data class App(
+    val id: Int,
+    val name: String,
+    val icon_url: String,
+    val short_description: String,
+    val full_description: String,
+    val category_id: Long,
+    val developer: String,
+    val age_rating: String,
+    val apk_url: String,
+    val screenshots: List<Screenshot>
+)
