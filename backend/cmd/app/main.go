6bdf09013c6ec1a8c04b84c17167f9e59876bfdb
UnemployedTocha <UnemployedTocha@mail.ru>
2025-10-05 10:28:43 +0700

    Add: http server init and app_id endpoint

--- backend/cmd/app/main.go
+++ backend/cmd/app/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"backend/internal/http-server/handlers"
+	k "backend/internal/kafka"
 	"backend/internal/repository"
 	"log/slog"
+	"net/http"
 	"os"
 
 	"github.com/go-chi/chi/v5"
@@ -26,11 +28,15 @@ func main() {
 		log.Error("Failed to connect to database")
 		os.Exit(1)
 	}
-	_ = repo
 	log.Info("Storage successfully started")
 
-	// init router
+	// start kafka consumer
+	// handlers := map[string]*k.Handler{"app_topic": han.NewAppMsgHandler(repo, log)}
+	consumer, err := k.NewConsumer("kafka:29091", repo, []string{"app_topic", "category_topic", "screenshot_topic"}, "123", log)
+	log.Info("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+	go consumer.Start()
 
+	// init router
 	router := chi.NewRouter()
 
 	router.Use(middleware.RequestID)
@@ -38,11 +44,21 @@ func main() {
 	router.Use(middleware.Recoverer)
 	router.Use(middleware.URLFormat)
 
-	// router.Route("/app", func(r chi.Router) {
-	// 	r.Get("/{}", handlers.NewGetAppHandler(repo, log)) // GET /app/{app_id} - конкретное приложение
-	// })
+	// setup routes
+	router.Route("/app", func(r chi.Router) {
+		r.Get("/{app_id}", handlers.NewGetAppHandler(repo, log)) // GET /app/{app_id}
+	})
 
-	//init server
+	// init http server
+	srv := &http.Server{
+		Addr:    ":8089",
+		Handler: router,
+	}
+
+	log.Info("HTTP server started on port 8098")
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Error("HTTP server error", slog.Any("err", err))
+	}
 
 }
 
